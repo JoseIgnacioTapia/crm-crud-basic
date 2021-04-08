fd9c1caaa903c45eecaa5e1d025bba8f4857c344
JoseIgnacioTapia <josignaciot@gmail.com>
2021-04-08 17:32:29 -0300

    POST to server with new client

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -1,4 +1,5 @@
 import { mostrarAlerta } from "./funciones.js";
+import { nuevoCliente } from "./API.js";
 
 (function () {
   const formulario = document.querySelector("#formulario");
@@ -26,7 +27,7 @@ import { mostrarAlerta } from "./funciones.js";
       return;
     }
 
-    console.log("Si se paso la validación");
+    nuevoCliente(cliente);
   }
 
   function validarObjeto(obj) {
