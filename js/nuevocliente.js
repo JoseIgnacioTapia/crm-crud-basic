7740d217bd49b693727fc87287aac8848d46a106
JoseIgnacioTapia <josignaciot@gmail.com>
2021-04-07 22:26:01 -0300

    Validation for new customers

---
+++ js/nuevocliente.js
@@ -0,0 +1,33 @@
+(function () {
+  const formulario = document.querySelector("#formulario");
+
+  formulario.addEventListener("submit", validarCliente);
+
+  function validarCliente(e) {
+    e.preventDefault();
+
+    const nombre = document.querySelector("#nombre").value;
+    const email = document.querySelector("#email").value;
+    const telefono = document.querySelector("#telefono").value;
+    const empresa = document.querySelector("#empresa").value;
+
+    const cliente = {
+      nombre,
+      email,
+      telefono,
+      empresa,
+    };
+
+    if (!validarObjeto(cliente)) {
+      // Mostrar mensaje
+      console.log("Todos los campos son obligatorios");
+      return;
+    }
+
+    console.log("Si se paso la validación");
+  }
+
+  function validarObjeto(obj) {
+    return Object.values(obj).every((input) => input !== "");
+  }
+})();
